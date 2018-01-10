86b7b6fd8dde2b3fb856f58151f3a3594276fe98
Eddy Caldas <eddycaldas@live.com>
2018-01-10 14:26:44 -0700

    creating elements

--- app.js
+++ app.js
@@ -54,3 +54,35 @@ val = listItem.previousSibling;
 val = listItem.nextElementSibling.nextElementSibling.previousElementSibling;
 
 console.log(val);
+
+// CREATING ELEMENTS-------------------------->
+
+// create element
+const li = document.createElement('li');
+
+// add class
+li.className = 'collection-item';
+
+// add ID
+li.id = 'new-item';
+
+// add attribute 
+li.setAttribute('title', 'New Item');
+
+// create text node and append
+li.appendChild(document.createTextNode('Hello worlddd'));
+
+// append li as child to ul
+document.querySelector('ul.collection').appendChild(li);
+
+// create new link element
+const link = document.createElement('a');
+// add classes
+link.className = 'delete-item secondary-content';
+// add icon html
+link.innerHTML = '<i class="fa fa-remove"></i>';
+// append link to li
+li.appendChild(link);
+
+
+console.log(li);
